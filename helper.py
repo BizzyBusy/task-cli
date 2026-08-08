e1740191cb73fccc4653ea4ee9134ae198e16670
Bisrat Abebe <bisratgabebe@gmail.com>
2026-08-07 22:38:49 -0400

    created helper.py to store future helper functions to be accessed in main.py

---
+++ helper.py
@@ -0,0 +1,17 @@
+#TODO 1: create helper function that checks the action requested in the user's arguments, with valid checks:
+
+    # Add, Update, and Delete tasks: add, update, delete
+
+    # Mark a task as in progress or done: mark-in-progress, mark-done
+
+    # List all tasks: list
+
+    # List all tasks that are done: list done
+
+    # List all tasks that are not started: list todo
+
+    # List all tasks that are in progress: list in-progress
+
+#TODO 2: write functions that update JSON file (and creates it if it doesn't exist) based on the actions defined above
+
+
