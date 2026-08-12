b0db75b1eed640c25385d29523d71bc62b3ac970
Bisrat Abebe <bisratgabebe@gmail.com>
2026-08-11 20:10:36 -0400

    began implementing testcases to stress test isValidOp

--- tests.py
+++ tests.py
@@ -9,12 +9,33 @@ class TestCheckerMethod(unittest.TestCase):
     def test_noArgs(self):
         """
         Testing that isValidOp correctly detects when no arguments are provided."""
-        
+
         input = [""]
 
         res = helper.isValidOp(input)
 
         self.assertFalse(res, "An input was falsely detected.")
+
+    def test_singleArg(self):
+        """
+        Testing that isValidOp correctly detects the "list" operation as the only valid single argument.
+        """
+            
+        single_args = ["list", "add", "update", "delete", "mark-done", "mark-in-progress"]
+
+        for arg in single_args:
+                self.assertEqual(helper.isValidOp(["", arg]), arg == "list", f'The single argument "{arg}" is incorrectly passed as valid.')
+
+    #TODO: COMPLETE THIS FUNCTION BEFORE MOVING ON TO FUTURE TESTCASES
+    def test_addValid(self):
+        """
+        Testing that isValidOp correctly detects a valid add operation.
+        """
+
+        bank = ["1", "task", "", " ", "hello ", " hello", " hello ", "@", "hello    world"]
+
+        for second_arg in bank:
+            self.assertTrue(helper.isValidOp(["", "add", second_arg]), f'The second argument "{second_arg}" led to the add operation incorrectly failing, despite being valid in length.')
 
 
 if __name__ == '__main__':
